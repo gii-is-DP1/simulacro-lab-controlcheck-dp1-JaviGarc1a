96a95deed7946867df97a29f19157ff9e4994a70
Javier García Aguilar <javiergarciaaguilar2002@gmail.com>
2022-11-03 11:14:07 +0100

    SimulacroCompleto

--- src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
@@ -3,13 +3,25 @@ package org.springframework.samples.petclinic.product;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 
-
-public interface ProductRepository {
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Integer> {
     List<Product> findAll();
+
+    @Query("SELECT pType FROM ProductType pType")
     List<ProductType> findAllProductTypes();
+
+    @Query("SELECT pt FROM ProductType pt WHERE pt.name = :name")
+    ProductType getProductType(@Param("name") String name);
+
+    @Query("SELECT p FROM Product p WHERE p.price < ?1")
+    List<Product> findByPriceLessThan(double cost);
+
     Optional<Product> findById(int id);
     Product findByName(String name);
     Product save(Product p);
